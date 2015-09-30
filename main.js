fdf1f175b2ec32c4e12b632c31ff37e65a7be810
cpollard1001 <cpollard1001@gmail.com>
2015-09-29 20:25:06 -0400

    Fixed typo which prevented the instrument from loading

--- main.js
+++ main.js
@@ -314,7 +314,7 @@ function createBolt(startStar, endStar){
 
 
 MIDI.loadPlugin({
-    soundfontUrl: ".assets/soundfonts/",
+    soundfontUrl: "assets/soundfonts/",
     instrument: "acoustic_grand_piano",
     onprogress: function(state, progress) {
         console.log(state, progress);
